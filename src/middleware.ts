9df6266bcbcd5ff18c12262e6b4d8a473b3fc4be
Faizullin <osmanfajzulla@gmail.com>
2025-07-25 17:18:46 +0500

    update-design

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,6 +19,7 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/', // Run middleware on index page
+    '/editor(.*)',
     '/(api|trpc)(.*)',
   ],
 };
